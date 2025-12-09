2b98e71e43e0a4bf3a01429e0660ebbffc3635be
NBx03 <hopm123dustier@gmail.com>
2025-12-09 13:03:31 +0700

    Добавил класс Parser (черновая реализация)

---
+++ src/main/java/ru/nsu/fit/lispmachine/parser/Parser.java
@@ -0,0 +1,20 @@
+﻿package ru.nsu.fit.lispmachine.parser;
+import ru.nsu.fit.lispmachine.domain.*;
+import java.util.*;
+public class Parser {
+    public LispValue parse(String input) {
+        // TODO: Реализовать нормальный разбор строк позже
+        String[] tokens = input.replace("(", " ( ").replace(")", " ) ").trim().split("\\s+");
+        return readFrom(new ArrayDeque<>(Arrays.asList(tokens)));
+    }
+    private LispValue readFrom(Queue<String> tokens) {
+        String token = tokens.poll();
+        if ("(".equals(token)) {
+            List<LispValue> list = new ArrayList<>();
+            while (!")".equals(tokens.peek())) list.add(readFrom(tokens));
+            tokens.poll();
+            return new LispCons(list.get(0), LispNil.INSTANCE); 
+        }
+        return new LispSymbol(token);
+    }
+}
